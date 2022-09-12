d154055197c955da71842bff10ff51ebcd0dcd96
maekyngrigs10 <107376573+maekyngrigs10@users.noreply.github.com>
2022-09-12 04:11:04 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,40 @@
+
+$('.btn1').click(function(){
+	$('.btn3').show();
+    $('.btn4').show();
+    $('.btn1').hide();
+    $('.btn2').hide();
+});
+$('.btn2').click(function(){
+    $('.btn3').show();
+    $('.btn4').show();
+    $('.btn1').hide();
+    $('.btn2').hide();
+});
+
+$('.btn3').click(function(){
+    $('.btn4').show();
+    $('.btn5').show();
+    $('.btn3').hide();
+    
+});
+
+$('.btn5').click(function(){
+    $('.secret').show();
+    $('.btn5').hide();
+    $('.btn4').hide();
+    $('.doit').show();
+});
+
+$('.secret').click(function(){
+    $('.gameintro').show();
+    $('.buttons').hide();
+    $('.secret').hide();
+    $('.doit').hide();
+});
+
+$('.start').click(function(){
+    $('.game').show();
+    $('.gameintro').hide();
+    $('.open').hide();
+});
